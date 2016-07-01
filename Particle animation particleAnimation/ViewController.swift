671f82174a7d5387c0c18bd2e633be5210099ecb
bingo <616800976@qq.com>
2016-07-01 20:35:56 +0800

    Initial Commit

---
+++ Particle animation particleAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Particle animation particleAnimation
+//
+//  Created by 李伟 on 16/7/1.
+//  Copyright © 2016年 李伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
